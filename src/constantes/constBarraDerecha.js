4d0827d49df2db420b49182348693b4443416ade
AlvaroVillanuevaLopez <alvarovillanuevalopez@gmail.com>
2026-03-10 10:40:41 +0100

    constantes insertadas

---
+++ src/constantes/constBarraDerecha.js
@@ -0,0 +1,34 @@
+export const SECCIONES={
+    contacto: "CONTACTO",
+    perfil_profesional: "PERFIL PROFESIONAL",
+    habilidades: "HABILIDADES",
+    tecnologias: "TECNOLOGÍAS",
+    idiomas: "IDIOMAS"
+}
+export const CONTACTODES={
+    location: "Madrid, España",
+    phone: "+34 650 37 05 36",
+    mail: "alvarovillanuevalopez@gmail.com",
+    linekdin: "Álvaro Villanueva López"
+}
+export const PERFILPROFESIONALDES={
+    descripcion: "Aficionado al ámbito de la informática, en búsqueda de una oportunidad para desarrollarme profesional y personalmente. Soy una persona interesada por el conocimiento, comprometido, respetuoso y disciplinado. Me motiva realizar un buen trabajo, aprendiendo de mis errores."
+}
+export const HABILIDADESDES={
+    equipo:"Trabajo en equipo",
+    creatividad: "Creatividad",
+    disciplina: "Disciplina",
+    asertividad: "Asertividad"
+}
+export const TECNOLOGÍASDES={
+    google: "Google Drive",
+    word: "Word, Excel",
+    sql: "SQL",
+    java: "Java y PHP",
+    ia: "Automatización con la IA",
+    html: "HTML, jQuery CSS y JavaScript"
+}
+export const IDIOMASDES={
+    spanish: "Español: Nativo",
+    english: "Inglés: Nivel Cambridge B1 y estancia en el extranjero"
+}
---
+++ src/constantes/constParteBlanca.js
@@ -0,0 +1,52 @@
+export const SECTIONS={
+    s1: "EXPERIENCIA LABORAL",
+    s2: "FORMACIÓN ACADÉMICA",
+    s3: "CERTIFICACIONES"
+}
+export const FECHAS={
+    f1: "2024-2025",
+    f2: "2023-2025",
+    f3: "2021-2023",
+    f4: "2024-2026",
+    f5: "2022-2024",
+    f6: "2020-2022"
+}
+export const DESCRIPCIONES={
+    d1: "Auxiliar en el departamento de soporte IT, algunas de mis responsabilidades fueron:",
+    d2: "Auxiliar en la zona de restauración, las responsabilidades que tuve fueron:",
+    d3: "Colocación del producto en el almacén y limpieza del mismo. Algunas tareas fueron: "
+}
+export const TAREAS={
+    t1: "Realización de maquetado de equipos para otros empleados. Explicando los softwares a utilizar y las funcionalidades del equipo.",
+    t2:"Automatización de preventivos y tareas rutinarias. Tanto de tareas diarias, semanales, quincenales y mensuales.",
+    t3: "Soporte técnico a empleados ante problemas de funcionalidad o de rotura de equipos.",
+    t4: "Gestión del inventario, realización de solicitudes, pedidos, guardado de los equipos que se podían utilizar y reciclaje de los equipos obsoletos.",
+    t5: "Atención al cliente, dentro de restaurante o en puesto, a las afueras del restaurante.",
+    t6: "Servicio de barra al cliente, con manejo de TPV y cobro al mismo.",
+    t7: "Limpieza e higienización de mi puesto de trabajo, antes, durante y después del servicio.",
+    t8: "Gestión del inventario, realización de solicitudes, pedidos, guardado de los equipos que se podían utilizar y reciclaje de los equipos obsoletos.",
+    t9: "Carga y descarga del pedido en el almacén.",
+    t10: "Empaquetado, colocación e inventariado de los productos tratados en el almacén.",
+    t11: "Soporte técnico a empleados ante problemas de funcionalidad o de rotura de equipos.",
+    t12: "Limpieza de máquinas, utensilios y desinfección de los mismos, como última parte del servicio."
+}
+export const TRABAJOS={
+    tr1:"AUXILIAR TÉCNICO DE INFORMÁTICA",
+    tr2: "AUXILIAR DE RESTAURACIÓN",
+    tr3: "MOZO DE ALMACÉN Y LIMPIEZA"
+}
+export const ESTUDIOS={
+    e1: "TÉCNICO SUPERIOR EN DESARROLLO DE APLICACIONES WEB",
+    e2: "TÉCNICO SUPERIOR EN ADMINISTRADOR DE SISTEMAS EN RED",
+    e3: "BACHILLERATO ESPECIALIDAD CIENCIAS TECNOLÓGICAS"
+}
+export const CENTROS={
+    ce1: "IES Domenico Scarlatti",
+    ce2: "IES Pio Baroja",
+    ce3: "Colegio Bilingüe Valle del Miro"
+}
+export const CERTIFICACIONES={
+    comun: "Certificado Curso OpenWebinars",
+    c1: "CERTIFICATION SQL FROM CERO TO ADVANCED",
+    c2: "CERTIFICATION PHP: EXPANDING CONCEPTS"
+}
