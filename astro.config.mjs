33ac1398c81121695685d052b4ed97f98c1a82d7
AlvaroVillanuevaLopez <alvarovillanuevalopez@gmail.com>
2026-03-12 09:13:31 +0100

    Configuración para despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // La integración oficial se encarga de todo, no necesitas plugins de vite
+  // Reemplaza con tu nombre de usuario y el nombre de tu repositorio
+  site: 'https://AlvaroVillanuevaLopez.github.io',
+  base: '/proyectos-Practicas',
   integrations: [tailwind()],
   devToolbar: {
     enabled: false,
